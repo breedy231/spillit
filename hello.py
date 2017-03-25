36a454f4d016d610e23329c68c44eeae07276e06
BReed <breed@carbonite.com>
2017-03-25 13:28:27 -0400

    Added stuff to hello.py

--- hello.py
+++ hello.py
@@ -11,7 +11,7 @@ from wtforms.validators import DataRequired
 
 indicoio.config.api_key = '426cd40e597f61242dba879063d99567'
 
-def get_emotion():
+def get_emotion(string):
 	user_input = input("What are you thinking about right now?")
 	print "Ok, you're thinking about " + str(user_input)
 	emotion = indicoio.emotion(user_input)
@@ -31,17 +31,18 @@ bootstrap = Bootstrap(app)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+	return render_template('index.html')
 
 @app.route('/success')
 def success():
-    return 'Success! Waiting for other players to join'
+    return 'Success! Waiting for other players to join.'
 
 @app.route('/submit', methods=('GET', 'POST'))
 def submit():
     form = MyForm()
     if form.validate_on_submit():
+    	get_emotion(form.value)
         return redirect('/success')
     return render_template('submit.html', form=form)
 
