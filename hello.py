b2e0e675ce7cfd65ed66e3e53bb1b8cd31565d02
BReed <breed@carbonite.com>
2017-03-25 14:05:45 -0400

    Changed port again

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import requests
 import indicoio
+import os
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask import render_template, request, flash, redirect
@@ -48,4 +49,5 @@ def submit():
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0',port=5000)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0',port=port)
