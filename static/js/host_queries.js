afe967e3fe1702708422ee0e57eeec049761f39d
Noah Appleby <appleby.n@husky.neu.edu>
2017-03-26 08:59:00 -0400

    remove bad request, added game ready states hide show

--- static/js/host_queries.js
+++ static/js/host_queries.js
@@ -1,10 +1,12 @@
 var users = [];
 var question, questionType;
 
-var socket = io.connect('http://' + document.domain + ':' + location.port);
+var socket = io.connect('http://' + document.domain + ':' + location.port, {secure: true});
+
 socket.on('connect', function() {
     socket.emit('message', "The host is connected!");
 });
+
 socket.on('response', function(data) {
     console.log(data);
     users.push(data);
@@ -14,6 +16,15 @@ socket.on('response', function(data) {
 $(document).ready(function() {
     socket.emit('questionRequest', 'host is requesting a question');
 });
+
+$('#startGameButton').click(function () {
+    socket.emit('startGame', 'host started game');
+})
+
+socket.on('startGame', function(data) {
+    $('#startGameButton').hide();
+});
+
 
 socket.on('questionSend', function(data) {
     console.log(data);
--- static/js/user_queries.js
+++ static/js/user_queries.js
@@ -1,31 +1,42 @@
 var userId, questionType;
 
-var socket = io.connect('http://' + document.domain + ':' + location.port);
+var socket = io.connect('http://' + document.domain + ':' + location.port, {secure: true});
+
 socket.on('connect', function() {
-    socket.emit('message', "Someone new is on the site");
+    socket.emit('message', "A user has opened the site");
 });
+
 socket.on('response', function(data) {
-    userId = data
+    userId = data;
     $("#userId").text(userId +"");
 });
 
-
 $(document).ready(function() {
-    $(".wait").hide();
+    $(".waitGame").hide();
+    $(".waitPlayers").hide();
+    $(".response").hide();
 });
 
 
 $('#addUserButton').click(function () {
     socket.emit('newUser', $('#nameTextField').val());
     $(".pre").hide();
-    $(".wait").show();
-})
+    $(".waitGame").show();
+});
 
 socket.on('questionSend', function(data) {
-      console.log("user " + data);
+    console.log("user " + data);
     questionType = data[1];
 });
 
-$('#addResponseButton').click(function () {
+$('#sendResponseButton').click(function () {
     socket.emit('newResponse', [userId, questionType, $('#responseTextField').val()]);
-})
+    $(".response").hide();
+    $(".waitPlayers").show();
+
+});
+
+socket.on('startGame', function(data) {
+    $(".waitGame").hide();
+    $(".response").show();
+});
