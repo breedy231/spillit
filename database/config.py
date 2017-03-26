6cd3fb893a4604b3c23b1e41526a483bf7e16afc
BReed <breed@carbonite.com>
2017-03-25 20:57:58 -0400

    Still moving stuff to get heroku working

--- database/config.py
+++ database/config.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from flask import Flask, g
 import sqlite3
+import spillit.models
 
 DATABASE = 'database/database.db'
 
@@ -25,5 +26,5 @@ def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
-    import spillit.models
+    
     Base.metadata.create_all(bind=engine)
--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import requests
 import indicoio
-import eventlet
+
 import os
 from flask_socketio import SocketIO
 from flask import Flask, g
@@ -11,7 +11,7 @@ from flask_socketio import SocketIO, emit, send
 from database.config import db_session
 from database.config import get_db, init_db
 from database.queries import *
-import spillit.models
+import models
 
 indicoio.config.api_key = '426cd40e597f61242dba879063d99567'
 
