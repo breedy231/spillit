8f2468a4c225e0e57df368778bb42bcbea635f16
BReed <breed@carbonite.com>
2017-03-25 20:08:19 -0400

    Added queries

--- database/queries.py
+++ database/queries.py
@@ -3,6 +3,10 @@ from database.config import db_session
 
 def handle_new_user(message):
     newUser = User(message);
-    if(User.query.filter(User.leader).count() == 0):
-        db_session.add(newUser, True);
+    if(User.query.filter(User.leader).count() == 1):
+        db_session.add(newUser);
+    else: 
+    	newUser.leader = True;
+    	db_session.add(newUser);
     db_session.commit();
+
